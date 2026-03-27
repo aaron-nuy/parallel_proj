8ed1ab50fe07b76028c332f0143248dc9b49212c
aaron <aaronjobac@gmail.com>
2026-03-27 13:28:06 +0100

    Add support for either sequential or parallel

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 #include "rand_utils.h"
 #include "simulation.h"
 #include "typedefs.h"
+#include <time.h>
 
+#ifdef PARALLEL
+#include <mpi.h>
+#endif
 
 const u32 GRID_WIDTH = 300;
 const u32 GRID_HEIGHT = 300;
@@ -17,18 +21,82 @@ const u32 ITERATIONS_TO_RUN = 365;
     return ((u64) hi << 32) | lo;
 }
 
+#ifdef SEQUENTIAL
 int main(int argc, char **argv)
 {
     rand_init(SEED);
 
+    u64 start_cycles, end_cycles;
+    struct timespec start_time, end_time;
+    f64 elapsed_time;
+
     auto simulation = simulation_create(TOTAL_INDIVIDUALS, ITERATIONS_TO_RUN, GRID_WIDTH, GRID_HEIGHT);
     simulation_populate(&simulation);
-    auto start = rdtsc();
+
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    start_cycles = rdtsc();
+
     simulation_run(&simulation);
-    auto end = rdtsc();
+
+    end_cycles = rdtsc();
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    elapsed_time =
+            (end_time.tv_sec - start_time.tv_sec) +
+            (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
+
     simulation_destroy(&simulation);
 
-    printf("Time taken to simulate was: %lu cycles\n", (u64) (end - start));
+    printf("Cycles taken to simulate were: %lu cycles\n", (u64) (end_cycles - start_cycles));
+    printf("Time taken to simulate was: %f seconds\n", elapsed_time);
 
     return 0;
 }
+#else
+int main(int argc, char **argv)
+{
+    rand_init(SEED);
+
+    u32 world_rank, world_size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    Simulation simulation;
+    u64 start_cycles, end_cycles;
+    struct timespec start_time, end_time;
+    f64 elapsed_time;
+
+    simulation = simulation_create(TOTAL_INDIVIDUALS, ITERATIONS_TO_RUN, GRID_WIDTH, GRID_HEIGHT);
+    simulation_populate(&simulation);
+
+    if (world_rank == 0)
+    {
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+        start_cycles = rdtsc();
+    }
+
+    simulation_run(&simulation);
+
+    if (world_rank == 0)
+    {
+        end_cycles = rdtsc();
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        elapsed_time =
+                (end_time.tv_sec - start_time.tv_sec) +
+                (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
+    }
+
+    simulation_destroy(&simulation);
+
+    MPI_Finalize();
+
+    if (world_rank == 0)
+    {
+        printf("Cycles taken to simulate were: %lu cycles\n", (u64) (end_cycles - start_cycles));
+        printf("Time taken to simulate was: %f seconds\n", elapsed_time);
+    }
+
+    return 0;
+}
+#endif
