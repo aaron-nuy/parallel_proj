d528db2507dfa1363a60b2d10be320119853008b
aaron <aaronjobac@gmail.com>
2026-04-02 21:03:08 +0200

    Use libc rand

--- rand_utils.c
+++ rand_utils.c
@@ -17,50 +17,17 @@ static int _rand_rptr = 0;
 
 void rand_init(u32 seed)
 {
-    if (seed == 0u)
-        seed = 1u;
-
-    _rand_table[0] = (i32) seed;
-
-    for (int i = 1; i < _RAND_DEG; i++)
-    {
-        i32 prev = _rand_table[i - 1];
-        i32 hi = prev / 127773;
-        i32 lo = prev % 127773;
-        i32 x = 16807 * lo - 2836 * hi;
-        if (x < 0)
-            x += 2147483647;
-        _rand_table[i] = x;
-    }
-
-    _rand_fptr = _RAND_SEP;
-    _rand_rptr = 0;
-
-    for (int i = _RAND_DEG * 10; i > 0; i--)
-        (void) rand_rand();
+    srand(seed)
 }
 
 [[nodiscard]] i32 rand_rand(void)
 {
-    _rand_table[_rand_fptr] += _rand_table[_rand_rptr];
-
-    i32 result = (_rand_table[_rand_fptr] >> 1) & 0x7FFFFFFF;
-
-    if (++_rand_fptr >= _RAND_DEG)
-    {
-        _rand_fptr = 0;
-        _rand_rptr++;
-    } else if (++_rand_rptr >= _RAND_DEG)
-    {
-        _rand_rptr = 0;
-    }
-
-    return result;
+    rand();
 }
 
 [[nodiscard]] i32 rand_i32_uniform(i32 min, i32 max)
 {
-    return min + (i32)(((u64)(u32)rand_rand() * (u32)(max - min + 1)) >> 32);
+    return min + rand() % (max - min + 1);;
 }
 
 [[nodiscard]] f64 rand_f64_uniform(f64 min, f64 max)
