aa83a30701f8946e3e7043acd62e258d77d04a31
aaron <aaronjobac@gmail.com>
2026-04-02 16:53:35 +0200

    Add C99 `stdbool.h` compatibility fallback

--- typedefs.h
+++ typedefs.h
@@ -19,4 +19,12 @@ typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t u8;
 
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+#include <stdbool.h>
+#else
+typedef u8 bool;
+#define true 1
+#define false 0
+#endif
+
 #endif //PARALLEL_PROJ_TYPEDEFS_H
